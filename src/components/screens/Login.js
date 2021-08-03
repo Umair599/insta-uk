3d40ff8d29489005ea87e6e69289f73d8d5c1f9f
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-03 21:32:27 +0500

    update login

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,53 +1,62 @@
 import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Image} from 'antd';
-import {InstagramFilled} from '@ant-design/icons';
+import {InstagramFilled, FacebookFilled} from '@ant-design/icons';
 import {fetchCode} from '../actions';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
 import LoginImage from '../images/login.jpg';
+import {Link, useParams, useLocation} from 'react-router-dom';
 import {INSTAGRAM_APP_ID, REDIRECT_URI} from '../apis/credentials';
-class Login extends Component {
-   constructor(props) {
-    super(props);
-    this.handleLoad = this.handleLoad.bind(this);
- }
-   state = {
-    size: 'large',
-    accessCode: '',
-  };
-  componentDidMount() {
-    window.addEventListener('load', this.handleLoad);
- }
-
- componentWillUnmount() { 
-   window.removeEventListener('load', this.handleLoad)  
- }
-
- handleLoad() {
-   this.setState({accessCode: window.location.pathname});
- }
-  handleInstaClick = (e) => {
+const Login = (props)=>{
+  const { search } = useLocation();
+  const searchParams = new URLSearchParams(search);
+  const queryCode = searchParams.get('code');
+  const [code, setCode]= useState(queryCode);
+  useEffect(()=>{
+if(code){
+  props.fetchCode(code);
+}
+  },[code]);
+  const handleInstaClick = e => {
     window.location = `https://api.instagram.com/oauth/authorize?client_id=${INSTAGRAM_APP_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
+    
   };
-  render() {
-    const { size } = this.state;
+  const newCodeButton = e =>{
+    props.fetchCode('AQDsvA0ICd0lRAZeTw9SvlSptQdMYSeuCT45BzB9nDwEa2rphoDGZBhzHHNOHLDKlRkfZsPUvMVN6zuLe4kZIzG40KbCJjvHSRDph_SunYCFOL7Q4cJfrqwIw4p_Pa814ICdcXX_5GTBvcgUFX8Ca3Q25ITD1tAeq9ZpYiZ2JiDWEFEtzUa3EpvixxXcPcgMVtTuIulVcK40l85nDtpwfoOES-Uot2OqPe3POXdBsqODWQ');
+  };
+  if(props.isSignedIn){
+    return (<View>
+<Text style={styles.welcome}>{props.access_token}</Text>
+<Text style={styles.welcome}>{props.user_id}</Text>
+      Loading...</View>);
+}
     return (
       <SafeAreaView>
         <View style={styles.container}>
         <Image preview={false} width={600} height={400} src={LoginImage}/>
           <Text style={styles.welcome}>Welcome to insta-uk</Text>
-          <Button type="primary" size="large" onClick={this.handleInstaClick}>
+          <Button type="primary" size="large" onClick={handleInstaClick}>
             <InstagramFilled width={'10rem'} />
             Login with Instagram
           </Button>
-{this.state.accessCode}
+          <Button type='primary' size='middle' onClick={newCodeButton}>
+            <FacebookFilled width={'10rem'} />
+            Login with Instagram
+          </Button>
+          
         </View>
       </SafeAreaView>
     );
-  }
+  
 }
-export default connect(null, {fetchCode})(Login);
+const mapStateToProps=(state)=>{
+  return {
+    isSignedIn: state.auth.isSignedIn,
+    access_token: state.auth.access_token,
+    user_id: state.auth.isSignedIn
+  };
+}
+export default connect(mapStateToProps, {fetchCode})(Login);
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
