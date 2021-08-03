4bb9946222045b3554412cc5dbb0684648669474
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-03 08:04:16 +0500

    Credentials modified

--- src/components/apis/credentials.js
+++ src/components/apis/credentials.js
@@ -1,5 +1,12 @@
 export const INSTAGRAM_APP_ID='381976333268643';
-export const REDIRECT_URI='https://insta-uk.netlify.app';
+export const REDIRECT_URI='https://www.instareactuk.com/';
 export const INSTAGRAM_APP_SECRET='e328d4028f5d4c764688814a0db59586';
 
 
+
+//My details
+//'381976333268643'
+//https://insta-uk.netlify.app
+//other
+//6201234567890178
+//https://httpstat.us/200/
