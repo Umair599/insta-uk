9b077da2fbfef591ecbb24b22fffc645f6b1b746
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-03 21:35:01 +0500

    update auth

--- src/components/reducers/authReducer.js
+++ src/components/reducers/authReducer.js
@@ -1,10 +1,13 @@
-import {SIGN_IN, SIGN_OUT} from '../actions/types';
+import {SIGN_IN, SIGN_OUT, FETCH_CODE} from '../actions/types';
 const INITIAL_STATE={
     isSignedIn: null,
-    userId: null
+    userId: null,
+    access_token: null
 };
 export default (state=INITIAL_STATE, action)=>{
 switch (action.type){
+    case FETCH_CODE:
+        return {...state, isSignedIn: true, access_token: action.payload.access_token, userId: action.payload.user_id};
     case SIGN_IN:
         return {...state, isSignedIn: true, userId: action.payload};
     case SIGN_OUT:
