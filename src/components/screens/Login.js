388545428c838329c0ff114162040eebc5e4c830
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-08 16:39:27 +0500

    Update Login

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,20 +1,26 @@
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import {Image} from 'antd';
-
+import {StyleSheet, View, Text, TouchableOpacity, Image, TextInput, Dimensions, KeyboardAvoidingView} from 'react-native';
+import React, {useEffect, useState, createRef} from 'react';
 import {fetchCode, fetchPosts} from '../actions';
 import {connect} from 'react-redux';
-import LoginImage from '../images/login.jpg';
+import LoginImage from '../images/social_media.png';
 import {useLocation} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {INSTAGRAM_APP_ID, REDIRECT_URI} from '../apis/credentials';
 import { faInstagram} from '@fortawesome/free-brands-svg-icons';
+import { faLock, faAt } from '@fortawesome/free-solid-svg-icons';
+import Loader from '../utilities/Loader';
+const {width, height} = Dimensions.get('window');
+import InstaImage from '../images/insta_image.png';
 const Login = (props)=>{
   const { search } = useLocation();
   const searchParams = new URLSearchParams(search);
   const queryCode = searchParams.get('code');
   const [code]= useState(queryCode);
- 
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [errortext, setErrortext] = useState('');
+  const [loading, setLoading] = useState(false);
+  const passwordInputRef = createRef();
   useEffect(()=>{
 if(code) {
   props.fetchCode(code); 
@@ -24,28 +30,80 @@ if(code) {
     console.log('Long Access = ', props.accessToken);
     if (props.accessToken) props.history.push(`/${props.userId}/?accessToken=${props.accessToken}`);
   }, [props.accessToken]);
-  const handleInstaClick = e => {
+  const handleLoginClick = e => {
+    setLoading(true);
     window.location = `https://api.instagram.com/oauth/authorize?client_id=${INSTAGRAM_APP_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
     
   };
   if (props.isSignedIn) props.history.push(`/${props.userId}/?accessToken=${props.accessToken}`);
     return (
         <View style={styles.container}>
-        <Image preview={false} width={700} height={400} src={LoginImage}/>
-          <Text style={styles.welcome}>Welcome to Insta-React-UK-App</Text>
-          <TouchableOpacity style={{cursor: 'pointer', flexWrap:'wrap'}} onPress={handleInstaClick} >
-            <View style={{flexDirection: 'row', justifyContent: 'center',alignContent:'center', backgroundColor: '#fafafa',
-          alignItems: 'center', marginVertical: 7, borderWidth: 2, width: '100%',
-           borderColor: 'black', borderRadius: 10, paddingHorizontal:5, paddingVertical:10 }}>
-             <FontAwesomeIcon icon={faInstagram} size="2x" style={{marginInline:12}}/>
-
-            <Text style={{fontSize: 15, fontWeight: '400'}}>Login with Instagram</Text>
+          <Loader loading={loading} />
+          <Text style={styles.welcome}>Welcome to Insta-UK App</Text>
+          <View style={{flexDirection: 'row', justifyContent:'space-evenly', flex:1}}>
+          <Image source={LoginImage} style={styles.loginImage} />
+          <View style={styles.loginSection}>
+          <KeyboardAvoidingView enabled>
+          <Image source={InstaImage} style={styles.brandImage} />
+          <View style={styles.SectionStyle}>
+          <FontAwesomeIcon icon={faAt} size="1x" style={{ alignSelf:'center'}}/>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={(UserEmail) =>
+                  setUserEmail(UserEmail)
+                }
+                placeholder="Enter Email"
+                placeholderTextColor="#8b9cb5"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordInputRef.current &&
+                  passwordInputRef.current.focus()
+                }
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
             </View>
+            <View style={styles.SectionStyle}>
+            <FontAwesomeIcon icon={faLock} size="1x" style={{ alignSelf:'center'}}/>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={(UserPassword) =>
+                  setUserPassword(UserPassword)
+                }
+                placeholder="Enter Password" //12345
+                placeholderTextColor="#8b9cb5"
+                keyboardType="default"
+                ref={passwordInputRef}
+                onSubmitEditing={Keyboard.dismiss}
+                blurOnSubmit={false}
+                secureTextEntry={true}
+                underlineColorAndroid="#f000"
+                returnKeyType="next"
+              />
+            </View>
+            {errortext !== '' ? (
+              <Text style={styles.errorTextStyle}>
+                {errortext}
+              </Text>
+            ) : null}
+            <TouchableOpacity activeOpacity={0.2} style={styles.loginButton} onPress={handleLoginClick} >
+             <FontAwesomeIcon icon={faInstagram} size="2x" style={{marginInline:15}}/>
+            <Text style={styles.loginText}>Login</Text>
           </TouchableOpacity>
+          <Text
+              style={styles.registerTextStyle}
+              onPress={() => console.log('SignUp')}>
+              New Here ? Register
+            </Text>
+            </KeyboardAvoidingView>
+      </View>
+      
+          </View>
         </View>
    
     );
-  
 }
 const mapStateToProps=(state)=>{
   return {
@@ -57,20 +115,85 @@ const mapStateToProps=(state)=>{
 export default connect(mapStateToProps, {fetchCode, fetchPosts})(Login);
 const styles = StyleSheet.create({
   container: {
+    flex:1, 
     justifyContent: 'center',
     flexDirection: 'column',
-    position: 'relative',
-    alignItems: 'center',
-    textAlign: 'center',
+    marginVertical: 10,
+    alignContent: 'center',
     backgroundColor: '#ffffff',
-
   },
   welcome: {
     fontSize: 30,
     textAlign: 'center',
-    marginTop: 5,
-    marginBottom: 5,
+    marginVertical: 5,
     color: '#000000',
   },
-  
+  loginImage:{
+    resizeMode: 'contain',
+    width: width*0.5,
+    height: height*0.7,
+  },
+  loginSection:{
+    flexDirection: 'column',
+    marginVertical: 10,
+    alignContent:'center',
+    backgroundColor:'#fafafa',
+    borderWidth: 1,
+    borderColor: 'black',
+    alignSelf:'center',
+    width: width*0.4,
+    borderRadius: 10,
+    padding: 5
+  },
+  brandImage:{
+    resizeMode: 'contain',
+    width: 100,
+    height: 50,
+    alignSelf:'center'},
+  SectionStyle: {
+    flexDirection: 'row',
+    marginTop: 20,
+    height: 40,
+    marginHorizontal:5,
+  },
+  inputStyle: {
+    flex:1,
+    flexShrink: true,
+    color: 'black',
+    paddingLeft: 4,
+    paddingRight: 2,
+    borderWidth: 1,
+    borderRadius: 10,
+    marginStart: 5
+  },
+  loginText:{
+    fontSize: 20, fontWeight: '500'
+  },
+  loginButton:{
+    flexDirection: 'row', 
+    justifyContent: 'center',
+    alignContent:'center',
+    backgroundColor: '#b2dffc',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    paddingHorizontal:40,
+    paddingVertical:10,
+    height: 40,
+    width: 120,
+    flex:1,
+    cursor: 'pointer',
+    flexWrap:'no-wrap', 
+    alignSelf:'center',
+    marginVertical:10,
+  },
+  registerTextStyle: {
+    color: 'blue',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+    alignSelf: 'center',
+    padding: 10,
+  },
 });
