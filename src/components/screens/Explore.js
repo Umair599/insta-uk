8b81e264493dcf5770c6757787f9e4555fbe66b3
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-11 19:17:30 +0500

    update explore

--- src/components/screens/Explore.js
+++ src/components/screens/Explore.js
@@ -1,69 +1,13 @@
+import {StyleSheet, View, Text} from 'react-native';
 import React, { useEffect, useState, useContext } from "react";
 const Explore = (props) => {
   const { user, updateProfile } = useContext();
   const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
   return (
-    <div>
-      {loading ? (
-        <Loader />
-      ) : posts.length ? (
-        <div className="grid_posts">
-          {posts.map((post) => (
-            <a href={`/posts/${post.id}`}>
-              <div className="image_1">
-                <img src={post.url} className="image_post" loading="lazy" />
-                <div className="middle1">
-                  <div className="text1">
-                    {!post.likes ? (
-                      <span>
-                        0{" "}
-                        <i
-                          class="fa fa-heart"
-                          aria-hidden="true"
-                          style={{ color: "white" }}
-                        ></i>
-                      </span>
-                    ) : (
-                      <span>
-                        {post.likes.length}{" "}
-                        <i
-                          class="fa fa-heart"
-                          aria-hidden="true"
-                          style={{ color: "white" }}
-                        ></i>
-                      </span>
-                    )}
-                    &nbsp;&nbsp;
-                    {!post.comments ? (
-                      <span>
-                        0{" "}
-                        <i
-                          class="fa fa-comment"
-                          aria-hidden="true"
-                          style={{ color: "white" }}
-                        ></i>
-                      </span>
-                    ) : (
-                      <span>
-                        {post.comments.length}{" "}
-                        <i
-                          class="fa fa-comment"
-                          aria-hidden="true"
-                          style={{ color: "white" }}
-                        />
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </a>
-          ))}
-        </div>
-      ) : (
-        <img src={Upload} className="no_post" />
-      )}
-    </div>
+<View>
+    <Text>No Feed Posts</Text>
+    </View>
   );
 };
 
