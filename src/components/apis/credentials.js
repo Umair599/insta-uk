69e7c31b07aa6af89d18987f8b5267c0add99f45
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-11 14:01:11 +0500

    update credentials

--- src/components/apis/credentials.js
+++ src/components/apis/credentials.js
@@ -1,3 +1,3 @@
 export const INSTAGRAM_APP_ID='381976333268643';
-export const REDIRECT_URI='https://insta-basic.herokuapp.com/register';
+export const REDIRECT_URI='https://www.instareactuk.com/register';
 export const INSTAGRAM_APP_SECRET='e328d4028f5d4c764688814a0db59586';
